3f8835e3c08c76805b69b94e7d7c894808451567
Soheil <django.course.prj@gmail.com>
2021-02-26 14:38:23 +0330

    step3

--- course/panel/models.py
+++ course/panel/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, username, password = None):
+        user = self.model(username = username)
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_superuser(self, username, password = None):
+
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+
+    username =  models.EmailField(unique = True)
+    is_teacher = models.BooleanField(default = False)
+    code = models.DecimalField(max_digits = 12, decimal_places = 0, default = 0)
+
+    is_staff = models.BooleanField( default = False)
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
